64b0829926f89a744d213ebc86e8c84a07529daa
Sietse van der Molen <sietse@vdmolen.eu>
2013-04-18 17:30:07 +0200

    set up first test for AdaWM_Randr
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- tests/src/adawm_randr_tests.adb
+++ tests/src/adawm_randr_tests.adb
@@ -1,18 +1,23 @@
-with Ahven;
+with Ahven; use Ahven;
 
-use Ahven;
+with AdaWM_Randr;
+with xab;
+with xab_types;
 
 package body AdaWM_Randr_Tests is
+   Global_X_Connection : constant xab_types.xab_connection_t 
+      := xab.xab_connect;
+
    procedure Initialize (T : in out Test) is
    begin
-      Set_Name (T, "AdaWM Randr Module");
+      Set_Name (T, "AdaWM Randr");
 
       Framework.Add_Test_Routine
-        (T, Test_Fake_Single_Screen'Access, "Fake single screen");
+        (T, Test_Fake_Single_Screen_Exception_Absence'Access, "Exception absence");
    end Initialize;
 
-   procedure Test_Fake_Single_Screen is
+   procedure Test_Fake_Single_Screen_Exception_Absence is
    begin
-      Assert (False, "Please write the first test");
-   end Test_Fake_Single_Screen;
+      AdaWM_Randr.Fake_Single_Screen (Global_X_Connection);
+   end Test_Fake_Single_Screen_Exception_Absence;
 end AdaWM_Randr_Tests;
--- tests/src/adawm_randr_tests.ads
+++ tests/src/adawm_randr_tests.ads
@@ -6,5 +6,5 @@ package AdaWM_Randr_Tests is
    procedure Initialize (T : in out Test);
 
 private
-   procedure Test_Fake_Single_Screen;
+   procedure Test_Fake_Single_Screen_Exception_Absence;
 end AdaWM_Randr_Tests;
