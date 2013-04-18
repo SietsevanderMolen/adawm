57ac7bd31068137cdc71f8f8fb4281f032191eaa
Sietse van der Molen <sietse@vdmolen.eu>
2013-04-18 17:02:57 +0200

    use xml_runner to generate xml from ahven
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- tests/src/test_adawm.adb
+++ tests/src/test_adawm.adb
@@ -1,9 +1,9 @@
 with Ahven.Framework;
-with Ahven.Text_Runner;
+with Ahven.XML_Runner;
 with AdaWM_Tests;
 
 procedure Test_AdaWM is
       Suite : Ahven.Framework.Test_Suite := AdaWM_Tests.Get_Test_Suite;
 begin
-      Ahven.Text_Runner.Run (Suite);
+      Ahven.XML_Runner.Run (Suite);
 end Test_AdaWM;
