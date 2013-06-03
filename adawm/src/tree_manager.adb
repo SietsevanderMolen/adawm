6a66ee0c7e63b0134362da9339db215978132f01
Sietse van der Molen <sietse@vdmolen.eu>
2013-06-03 23:52:24 +0200

    add init to tree_manager
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- adawm/src/tree_manager.adb
+++ adawm/src/tree_manager.adb
@@ -1,7 +1,23 @@
+with Log;
+with X11_Root_Windows;
+
 package body Tree_Manager is
    procedure Add_Output_Container (output : Output_Containers.Output_Container)
    is
    begin
       null;
    end Add_Output_Container;
+
+   procedure Init_Tree
+   is
+      Root : Trees.Containers.Cursor := Tree.Root;
+      X11_Win : X11_Root_Windows.X11_Root_Window;
+   begin
+      Log.Increase_Indent;
+      Log.Info ("Creating tree");
+      Log.Info ("Adding X11_Root_Window to tree");
+      Tree.Append_Child (Parent   => Root,
+                         New_Item => X11_Win);
+      Log.Decrease_Indent;
+   end Init_Tree;
 end Tree_Manager;
--- adawm/src/tree_manager.ads
+++ adawm/src/tree_manager.ads
@@ -5,4 +5,5 @@ package Tree_Manager is
    Tree : Trees.Containers.Tree;
 
    procedure Add_Output_Container (output : Output_Containers.Output_Container);
+   procedure Init_Tree;
 end Tree_Manager;
