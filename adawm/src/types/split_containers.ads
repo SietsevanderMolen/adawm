84a06c383b32b1ff19fca020cdd5dd7156ec7ede
Jacob Procee <sn125164@gmail.com>
2013-06-03 22:31:23 +0200

    create constructors split_containers
    Signed-off-by: Jacob Procee <sn125164@gmail.com>

--- adawm/src/types/split_containers.ads
+++ adawm/src/types/split_containers.ads
@@ -1,15 +1,26 @@
 with Cons;
+with Trees;
 with Rectangles;
+with Workspaces;
 with xab_types;
 
 package Split_Containers is
    type Split_Container;
    type Split_Container_Access is access all Output_Container;
 
+   function Make (Tree : in out Trees.Containers.Tree; 
+                  Parent : Workspaces.Workspace) return Split_Container;
+   function Make (Tree : in out Trees.Containers.Tree;
+                  Parent : Workspaces.Workspace;
+                  Layout : Layout_Type) return Split_Containers;
+
+   type Layout_Type is (L_DEFAULT, L_STACKED, L_TABBED, L_DOCKAREA, L_OUTPUT,
+      L_SPLITV, L_SPLITH);
+
    type Split_Container is new Cons.Con with private;
 private
    type Split_Container is new Cons.Con with
       record
-         Todo : Boolean;
+         Layout : Layout_Type;
       end record;
 end Split_Containers;
