6c3aa589aeead5876d44c1398d4508886a8286f4
Sietse van der Molen <sietse@vdmolen.eu>
2013-06-03 19:48:07 +0200

    redo con and x11_win
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

---
+++ adawm/src/types/x11_root_windows.ads
@@ -0,0 +1,13 @@
+with Cons;
+
+package X11_Root_Windows is
+   type X11_Root_Window;
+   type X11_Root_Window_Access is access all X11_Root_Window;
+
+   type X11_Root_Window is new Cons.Con with private;
+private
+   type X11_Root_Window is new Cons.Con with
+      record
+         Heu : Boolean;
+      end record;
+end X11_Root_Windows;
