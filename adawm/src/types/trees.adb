7876c7ba2d6c0292f43e320c36216dd806ccb747
Jacob Procee <sn125164@gmail.com>
2013-06-03 19:21:49 +0200

    started porting trees
    Signed-off-by: Jacob Procee <sn125164@gmail.com>

--- adawm/src/types/trees.adb
+++ adawm/src/types/trees.adb
@@ -6,4 +6,15 @@ package body Trees is
    begin
       return T;
    end Create;
+   procedure Render(T: in out Trees.Tree) is
+   begin
+      -- Skipped croot null check because this is not the responsibility of this procedure
+      Log.Info ("Starting rendering");
+      -- mark_unmapped is refactored to Unmap_All
+      Log.Info ("Stopping rendering");
+   end;
+   procedure Unmap_All(T: in out Trees.Tree) is
+   begin
+      null;
+   end;
 end Trees;
