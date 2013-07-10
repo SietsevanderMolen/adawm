cceb7581b8c88950a3f37d19c88be8b377296fbc
Sietse van der Molen <sietse@vdmolen.eu>
2013-07-10 13:27:58 +0200

    add gpl3 license
    Signed-off-by: Sietse van der Molen <sietse@vdmolen.eu>

--- adawm/src/types/trees.ads
+++ adawm/src/types/trees.ads
@@ -1,3 +1,23 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                   Copyright (C) 2012-, Sietse van der Molen		     --
+--                                                                           --
+--    This file is part of AdaWM.			 	   	     --
+--									     --
+--    AdaWM is free software: you can redistribute it and/or modify          --
+--    it under the terms of the GNU General Public License as published by   --
+--    the Free Software Foundation, either version 3 of the License, or      --
+--    (at your option) any later version.                                    --
+--   		 		 	 	 	 	 	     --
+--    AdaWM is distributed in the hope that it will be useful,               --
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of         --
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          --
+--    GNU General Public License for more details.                           --
+--                                                                           --
+--    You should have received a copy of the GNU General Public License      --
+--    along with AdaWM.  If not, see <http://www.gnu.org/licenses/>.         --
+--                                                                           --
+-------------------------------------------------------------------------------
 with Ada.Containers.Indefinite_Multiway_Trees; use Ada.Containers;
 
 with Cons; use Cons;
